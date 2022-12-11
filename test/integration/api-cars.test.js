7781f2e0f4993058b85d2f57d5953cef37ff14db
Rafael Soares <r.soares2015@outlook.com>
2022-12-10 22:29:03 -0300

    test: Criacao dos testes do endpoint /cars/:id

--- test/integration/api-cars.test.js
+++ test/integration/api-cars.test.js
@@ -10,20 +10,40 @@ const { expect } = chai;
 chai.use(chaiHttp);
 
 describe('Testando a API', function () {
-    beforeEach(function () {
-        sinon.stub(fs, 'readFile').resolves(mockFile);
-        sinon.stub(fs, 'writeFile').resolves();
-    });
+  beforeEach(function () {
+    sinon.stub(fs, 'readFile').resolves(mockFile);
+    sinon.stub(fs, 'writeFile').resolves();
+  });
 
-    afterEach(function () {
-        sinon.restore();
-    });
-    describe('Usando o método GET em /cars', function () {
-        it('Retorna todos os carros', async function () {
-            const response = await chai.request(app).get('/cars');
+  afterEach(function () {
+    sinon.restore();
+  });
+  describe('Usando o método GET em /cars', function () {
+    it('Retorna todos os carros', async function () {
+      const response = await chai.request(app).get('/cars');
 
-            expect(response).to.have.status(200);
-            expect(response.body).to.deep.equal(mockFile.cars);
-        });
+      expect(response).to.have.status(200);
+      expect(response.body).to.deep.equal(mockFile.cars);
     });
+  });
+  describe('Usando o método GET em /cars/:id', function () {
+    it('Retorna o carro com o id 3', async function () {
+      const expectedResponse = {
+        id: 3,
+        name: 'Gol',
+        brandId: 1,
+        fuelId: 3,
+      };
+      const response = await chai.request(app).get('/cars/3');
+
+      expect(response).to.have.status(200);
+      expect(response.body).to.deep.equal(expectedResponse);
+    });
+    it('Retorna uma lista vazia se for passado um id inválido', async function () {
+        const response = await chai.request(app).get('/cars/1000');
+
+        expect(response).to.have.status(404);
+        expect(response.body).to.deep.equal({ error: 'Car not found' });
+    });
+  });
 });
